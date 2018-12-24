7823ae1661493ffd159e30e5b4767f42473481de
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-24 13:38:00 +0900

    Next.js Link routing & lazy-loading

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 const authPage = () => {
   return (
     <div>
       <h1>Auth Page</h1>
+      <p>Go back to <Link href='/'><a>Home</a></Link>.</p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 const indexPage = () => {
   return (
     <div>
       <h1>Index Page</h1>
+      <p>Go to <Link href='/auth'><a>Auth</a></Link> to log in.</p>
     </div>
   );
 };
