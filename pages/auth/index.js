5d21929af4bcefc650ecfbc4bbce9d935b37ef46
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-24 14:24:35 +0900

    Next.js lifecycle hook: getInitialProps

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import Router from 'next/router';
 
-const authPage = () => {
+const authPage = props => {
   return (
     <div>
-      <h1>Auth Page</h1>
+      <h1>Auth Page - {props.asyncData}</h1>
       <button onClick={() => Router.push('/')}>Go back</button>
     </div>
   );
+};
+
+authPage.getInitialProps = context => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({asyncData: 'Hello World!'});
+    }, 1000);
+  });
+  return promise;
 };
 
 export default authPage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 
-const indexPage = () => {
-  return (
-    <div>
-      <h1>Index Page</h1>
-      <p>Go to <Link href='/auth'><a>Auth</a></Link> to log in.</p>
-      <p>Or go to <Link href='/profile'><a>Profile</a></Link>.</p>
-    </div>
-  );
-};
+class IndexPage extends Component {
+  static async getInitialProps (context) {
+    // will run on the server and client
+    // e.g. fetch data from database
+    // and pre-populate the app prior to rendering
+    // use await to wait for promise results
+    return {appName: 'Async-App-Name'};
+  }
 
-export default indexPage;
+  render () {
+    return (
+      <div>
+        <h1>Index Page - {this.props.appName}</h1>
+        <p>Go to <Link href='/auth'><a>Auth</a></Link> to log in.</p>
+        <p>Or go to <Link href='/profile'><a>Profile</a></Link>.</p>
+      </div>
+    );
+  }
+}
+
+export default IndexPage;
