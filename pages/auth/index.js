36b7e4aab2255fae5ee9444697cc9199178eeacb
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-12-24 13:41:15 +0900

    Next.js Router for imperative routing

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
+import Router from 'next/router';
 
 const authPage = () => {
   return (
     <div>
       <h1>Auth Page</h1>
-      <p>Go back to <Link href='/'><a>Home</a></Link>.</p>
+      <button onClick={() => Router.push('/')}>Go back</button>
     </div>
   );
 };
